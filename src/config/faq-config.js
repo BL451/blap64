a069234bc41a02b60bddea759a1d99809b1390d8
Benjamin Lappalainen <b.m.lappalainen@gmail.com>
2025-08-08 18:25:13 +0800

    OpenGraph tags and image added. Lore added to Site Guide.

--- src/config/faq-config.js
+++ src/config/faq-config.js
@@ -8,15 +8,19 @@ export const faqConfig = [
     },
     {
         question: "Web Presence",
-        answer: "Like what you see on this website? <a href=mailto:hello@blap64.xyz>Email me</a> and let's discuss what I can build for you!"
+        answer: "Like what you see on this website? <a href=mailto:hello@blap64.xyz>Email me</a> and let's discuss what I can build for you."
+    },
+    {
+        question: "Lore",
+        answer: "Benjamin Lappalainen -> B. Lappalainen -> BLAP. Years ago when I first made an Instagram account for my photography, I decided on @blapphoto as a username. 'blap' stuck ever since, and some of my friends have adopted it as a nickname for me.<br><br>As for '64':<br>We are in the era of 64-bit computers (generally, computers with size 2^64 integer and addressing registers).<br><br>64 is 8^2. 8 in Cantonese (in addition to other Chinese-speaking cultures) is considered a lucky number because it bears homophonic similarity to the word for 'wealth' or 'prosperity'.<br><br>64 is 2^6. I was 26 when I made the decision to choose a different path through life, which has since led me to some extraordinary people, places, ideas, and opportunities."
     },
     {
         question: "Technology (for the nerds)",
         answer: "The dynamic pages on this site are constructed using a creative coding library called <a href=\"https://p5js.org\" target=\"_blank\">p5.js</a>. I'm a big advocate for this library and teach people how to make art with it, as well as teach programming using it as a starting point. Otherwise, the site was built in HTML, CSS, and vanilla JavaScript. <a href=\"https://parceljs.org/\">Parcel</a> is the bundler, and the site is deployed to <a href=\"https://pages.github.com/\">GitHub Pages</a> so that my only expense is the domain."
     },
     {
-        question: "Isn't this a bit much?",
-        answer: "If you don't like it, it's not for you. <br><br> A 'brutalist' version of the site will be available soon."
+        question: "This is a bit much for me.",
+        answer: "If you don't like it, it's not for you. <br><br> However, a 'brutalist' version of the site will be available soon with the same content in a utilitarian package."
     }
 ];
 
